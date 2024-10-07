428a7847e318a9bbe41feba9a7bac18468076a7f
Agathe-Anselme <agathe.anselme@gmail.com>
2024-10-07 10:31:37 +0200

    ajout dans le fichier script.js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 let js = "amazing";
 if (js === "amazing") alert("Javascript is fun!");
 
-40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Agathe";
+console.log(firstName)
+console.log(firstName)
