227391f9503cd43049bddf12c932e2b23bfd455a
Agathe-Anselme <agathe.anselme@gmail.com>
2024-10-07 11:32:56 +0200

    Apprentissage des variables

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 let js = "amazing";
 if (js === "amazing") alert("Javascript is fun!");
 
+/*
+//Introduction to console.log
 console.log(40 + 8 + 23 - 10);
-
 console.log("Jonas");
 console.log(23);
 
+//Introduction to variables
 let firstName = "Agathe";
-console.log(firstName)
-console.log(firstName)
+console.log(firstName);
+console.log(firstName);
+
+// Variable name conventions
+let myFirstJob = "Farmer";
+let myCurrentJob = "Programmer";
+
+//Declaration of variables
+let country = "France";
+let continent = "Europe";
+console.log(country);
+console.log(continent);
+*/
