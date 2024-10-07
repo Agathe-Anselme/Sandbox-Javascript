160bea14b2833e1289f51028526a7deb67509bea
Agathe-Anselme <agathe.anselme@gmail.com>
2024-10-07 16:35:59 +0200

    Ajout de code dans le fichier script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ let firstName = "Agathe";
 console.log(firstName);
 console.log(firstName);
 
-// Variable name conventions
+// Variable name conventions : Camel case
 let myFirstJob = "Farmer";
 let myCurrentJob = "Programmer";
 
@@ -21,4 +21,45 @@ let country = "France";
 let continent = "Europe";
 console.log(country);
 console.log(continent);
+
+// Data type in Javascript
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Jonas");
+let year;
+console.log(year);
+console.log(typeof year);
+year = 1991;
+console.log(typeof year);
+
+// Mutating variables
+let age = 30;
+age = 31;
+
+//const or let ?
+const birthYear = 1991;
 */
+
+// Operators
+const ageAgathe = 2024 - 1984; 
+console.log(ageAgathe);
+let now = 2024;
+const ageMilo = now - 2019;
+const ageSacha = now - 2022;
+console.log(ageAgathe, ageMilo, ageSacha);
+
+console.log(ageAgathe *2, ageAgathe / 10, 2 ** 3)
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+// Concatenation
+const firstName = "Agathe";
+const lastName = "Anselme";
+console.log(firstName + " " + lastName)
+
+let x = 10 + 5; // 15
+x += 10; // x = x + 10
+x *= 4; // x = 
+console.log(x);
