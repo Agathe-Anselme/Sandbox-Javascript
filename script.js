77ca9880f47999cbcd942ae477d7f93024b88d74
Agathe-Anselme <agathe.anselme@gmail.com>
2024-10-21 14:46:23 +0200

    Ajout au script.js

--- script.js
+++ script.js
@@ -41,7 +41,6 @@ age = 31;
 
 //const or let ?
 const birthYear = 1991;
-*/
 
 // Operators
 const ageAgathe = 2024 - 1984; 
@@ -59,7 +58,65 @@ const firstName = "Agathe";
 const lastName = "Anselme";
 console.log(firstName + " " + lastName)
 
+//Assignment operators
 let x = 10 + 5; // 15
-x += 10; // x = x + 10
-x *= 4; // x = 
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--;
 console.log(x);
+
+// Comparison operators
+console.log(ageAgathe > ageMilo); // >, <, >=, <=
+console.log(ageMilo >= 18);
+console.log(now - 1991 > now - 2018);
+
+const firstName = "Agathe";
+const job = "developer";
+const birthYear = "1984";
+const year = 2024
+const Agathe = "I'm" + " " + firstName + ", a" + " " + (year - birthYear) + " year old" + " " + job;
+console.log(Agathe);
+
+const agatheNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}`;
+console.log(agatheNew);
+
+console.log(`Just a regular string...`)
+
+console.log('String with \n\ multiple \n\ lines ')
+
+const age = 15;
+
+if(age>=18) {
+    console.log('Sarah can start driving license');
+} else {
+    const yearsLeft = 18 - age;
+    console.log(`Go back home. Wait another ${yearsLeft} years.`);
+}
+
+const birthYear = 1984;
+
+let century;
+if(birthYear <= 2000) {
+    century = 20;
+}  else {
+    century = 21;
+}
+console.log(century);
+*/ 
+
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(BMIMark, BMIJohn);
+
+if (BMIMark > BMIJohn) {
+    console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`)
+} else {
+    console.log(`John's BMI is higher than Mark's!`)
+}
+
